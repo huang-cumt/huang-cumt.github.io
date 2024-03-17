94855bed2a23e7de8698377218fd4a71123c0f9c
HuangXinyu <hxy2317404325@163.com>
2024-03-17 19:46:50 +0800

    1

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1710666033',resources: [
+    cacheName: 'chirpy-1710675991',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
