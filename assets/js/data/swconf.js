45855b054a365e60fefbc47a1e6ce330731b689e
root <hxy2317404325@163.com>
2024-04-02 17:17:39 +0800

    20240402+UpdateBlog

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1712049409',resources: [
+    cacheName: 'chirpy-1712049459',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
