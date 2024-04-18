8f2744496dd5f5886de2245bd4d456a1a2cbea88
huang-cumt <hxy2317404325@163.com>
2024-04-18 08:36:36 +0800

    20240418+UpdateBlog

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1712898911',resources: [
+    cacheName: 'chirpy-1713400593',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
